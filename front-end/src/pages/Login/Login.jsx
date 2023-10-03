baa6b59b6f04d415505be3b2164263e529068e46
Kneel-soN <mnpdinoso01@tip.edu.ph>
2023-10-04 06:16:15 +0800

    Connect Solana Wallet

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,6 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Connection, PublicKey, clusterApiUrl  } from '@solana/web3.js';
+import { Program, AnchorProvider, web3, } from '@project-serum/anchor';
+import * as Web3 from '@solana/web3.js';
+
+
+const rpcUrl = 'https://api.testnet.solana.com';
+
+const opts = {
+  preflightCommitment:"processed",
+}
+
+const network = clusterApiUrl("testnet") // for testnet
+
+const getProvider = () => {
+  const connection = new Connection(network, opts.preflightCommitment);
+  const provider = new AnchorProvider(
+    connection,
+    window.solana,
+    opts.preflightCommitment
+  );
+  return provider;
+};
+
+
+
 
 const Login = () => {
   const navigate = useNavigate();
@@ -9,6 +34,26 @@ const Login = () => {
     username: "",
     password: "",
   });
+
+  const [solAdd, setsolAdd] = useState("");
+
+  const connectWallet = async () => {
+    if (!window.solana) {
+      alert("Solana wallet not found. Please install Sollet or Phantom extension.");
+      return;
+    }
+  
+    try {
+      await window.solana.connect();
+      const provider = getProvider();
+      const walletAddress = provider.wallet.publicKey.toString();
+      console.log("Wallet Address:" ,walletAddress)
+      setsolAdd(walletAddress);
+    } catch (err) {
+      console.error("Error connecting wallet:", err);
+    }
+  };
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,7 +82,10 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+  
+    <>
+
+<form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
         <input
@@ -57,7 +105,16 @@ const Login = () => {
         />
       </div>
       <button type="submit">Login</button>
+
     </form>
+
+
+    <form onSubmit={connectWallet}>
+   
+      <button type="submit">Connect Wallet</button>
+      <p>WalletAddress: {solAdd}</p>
+    </form>
+    </>
   );
 };
 
